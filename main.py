ad9612c89e1d91a9a4f27e1aef4e2e9ae3d7dc21
Robo Gombár <robo.gombar@gmail.com>
2025-04-22 13:11:50 +0000

    final report print done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_letters
+from stats import count_words, count_letters, create_list_of_counts
 
 def get_book_text(file_path):
     with open(file_path, encoding='utf-8') as f:
@@ -13,5 +13,24 @@ def get_book_text(file_path):
 book_text = get_book_text('books/frankenstein.txt')
 letters = count_letters(book_text)
 
-print(f'{count_words(book_text)} words found in the document')
-print(letters)
+# print(f'{count_words(book_text)} words found in the document')
+# print(letters)
+
+final_list = create_list_of_counts(letters)
+
+print(f'{" BOOKBOT ":=^33}')
+print('Analyzing book found at books/frankenstein.txt...')
+print(f'{" Word Count ":=^33}')
+print(f'Found {count_words(book_text)} total words')
+print(f'{" Character Count ":=^33}')
+
+final_list = create_list_of_counts(letters)
+
+for i in range(len(final_list)):
+    if final_list[i]['letter'].isalpha() == True:
+        print(f"{final_list[i]['letter']}: {final_list[i]['count']}")
+
+print(f'{" END ":=^33}')
+
+
+
--- stats.py
+++ stats.py
@@ -10,3 +10,17 @@ def count_letters(txt):
         else:
             dict[l.lower()] = 1
     return dict
+
+def create_list_of_counts(dict):
+    list_of_count = []
+    for key, value in dict.items():
+        letter_dict = {}
+        letter_dict['letter'] = key
+        letter_dict['count'] = value
+        list_of_count.append(letter_dict)
+
+    sorted_list_of_count = sorted(list_of_count, key=lambda item: item['count'], reverse=True)
+    
+    return sorted_list_of_count
+
+
